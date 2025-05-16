729714f5671f4dcc0014a9fff8a6f145535642ce
Owxnbrr <146349473+Owxnbrr@users.noreply.github.com>
2025-05-16 15:21:23 +0200

    Add files via upload

--- asset/js/script-imp.js
+++ asset/js/script-imp.js
@@ -4,3 +4,18 @@ const burger = document.querySelector('.burger');
     burger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
     });
+
+    const progressBar = document.querySelector('.timeline-progress');
+    const timeline = document.querySelector('.timeline');
+
+    window.addEventListener('scroll', () => {
+        const timelineTop = timeline.offsetTop;
+        const timelineHeight = timeline.offsetHeight;
+        const scrollY = window.scrollY + window.innerHeight;
+
+        // Calcul du pourcentage de scroll dans la timeline
+        const distance = scrollY - timelineTop;
+        const progress = Math.min(distance / timelineHeight, 1);
+
+        progressBar.style.height = `${progress * 100}%`;
+    });
